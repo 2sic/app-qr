50ac5104dad2066c778084b80776db58c5b70caa
Maxim Hüsler <maxim.huesler@2sic.com>
2021-10-08 09:03:17 +0200

    improvements models

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,19 +1,26 @@
 let QRCode = require('../../node_modules/qrcode-svg');
 
+interface QrCodeOptions {
+    color: string;
+    height: number;
+    width: number;
+}
 
-const winAny = (window as any);
-winAny.appQr2 ??= {};
-winAny.appQr2.init ??= init;
-
-export function init({ domId }: { domId: string }) {
+export function init({ domId, options }: { domId: string, options: QrCodeOptions }) {
     let qrDomObject = document.querySelector(`.${domId}`);
     let qrCode = new QRCode({
-        content: qrDomObject.getAttribute("qrContent"),
-        container: "svg-viewbox",
-        color: qrDomObject.getAttribute("qrColor"),
+        content: qrDomObject.getAttribute("href"),
+        color: options.color,
+        width: options.width,
+        height: options.height,
         join: true,
         ecl: "L",
-        padding: 0
+        padding: 0,
     });
     qrDomObject.innerHTML = qrCode.svg();
 }
+
+// so it can be called from the HTML when content re-initializes dynamically
+const winAny = (window as any);
+winAny.appQr2 ??= {};
+winAny.appQr2.init ??= init;
