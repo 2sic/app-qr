daa7e4aebe1518489def989c65ebf0267bb1c97b
Jeremy Farrance <jfarrance@accuraty.com>
2024-01-12 22:02:36 -0600

    Added TS version of downloadSvg() function

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,6 @@
 let QRCode = require('../../node_modules/qrcode-svg');
 
-interface QrCodeOptions {
+interface QrCodeOptions { 
     color: string;
     height: number;
     width: number;
@@ -20,7 +20,22 @@ export function init({ domId, options }: { domId: string, options: QrCodeOptions
     qrDomObject.innerHTML = qrCode.svg();
 }
 
+export function downloadSvg({ domClass, fileName }: { domClass: string, fileName: string }) {
+    let qrDomObject = document.querySelector(`.${domClass}`);
+    let svgData = qrDomObject.innerHTML;
+    let blob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    let url = URL.createObjectURL(blob);
+
+    let downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = fileName;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
 // so it can be called from the HTML when content re-initializes dynamically
 const winAny = (window as any);
 winAny.appQr2 ??= {};
 winAny.appQr2.init ??= init;
+winAny.appQr2.downloadSvg ??= downloadSvg;
